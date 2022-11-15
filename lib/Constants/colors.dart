ebf1a5676d3b4a2dacf06be2f785b47af2b3d2dd
shahbazakon <shahbajakon@gmail.com>
2022-11-15 10:36:59 +0530

    1. ScreenShorts added 2. Constants Colors added

---
+++ lib/Constants/colors.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static Color green = const Color.fromRGBO(56, 200, 117, 1);
+  static Color orange = const Color.fromRGBO(255, 105, 5, 1);
+  static Color blue = const Color.fromRGBO(61, 92, 255, 1);
+  static Color white = const Color.fromRGBO(255, 255, 255, 1);
+  static Color litePink = const Color.fromRGBO(255, 235, 240, 1);
+  static Color liteGreen = const Color.fromRGBO(234, 234, 255, 1);
+}
+
+class AppTextColor {
+  static Color blackAccent = const Color.fromRGBO(31, 31, 57, 1);
+  static Color grey = const Color.fromRGBO(133, 133, 151, 1);
+  static Color greyLite = const Color.fromRGBO(184, 184, 210, 1);
+  static Color whiteAccent = const Color.fromRGBO(244, 243, 253, 1);
+}
